39a5e2aa4b8920dd3ba188752d173a667aef5645
kuzminv <coolherringbone@gmail.com>
2024-07-09 18:54:41 +0700

    update Test

--- diwef_test.go
+++ diwef_test.go
@@ -1,11 +1,16 @@
 package diwef
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestInit(t *testing.T) {
 	initEmptyConfig(t)
 	initOneConfig(t)
 	initManyConfig(t)
+
+	errorCreatPath(t)
 }
 
 func initEmptyConfig(t *testing.T) {
@@ -24,6 +29,15 @@ func initEmptyConfig(t *testing.T) {
 
 	if log.config.LiveTime != DefaultLiveTime {
 		t.Errorf("config.LiveTime did not accept default value")
+	}
+
+	_, err = os.Stat(log.config.Path)
+	if err == nil {
+		os.Remove(log.config.Path)
+	} else if os.IsNotExist(err) {
+		t.Errorf("Path was not created")
+	} else {
+		t.Errorf(err.Error())
 	}
 }
 
@@ -52,6 +66,15 @@ func initOneConfig(t *testing.T) {
 	if log.config.LiveTime != liveTime {
 		t.Errorf("config.LiveTime did not accept the specified value")
 	}
+
+	_, err = os.Stat(path)
+	if err == nil {
+		os.Remove(path)
+	} else if os.IsNotExist(err) {
+		t.Errorf("Path was not created")
+	} else {
+		t.Errorf(err.Error())
+	}
 }
 
 func initManyConfig(t *testing.T) {
@@ -69,5 +92,15 @@ func initManyConfig(t *testing.T) {
 		LiveTime: liveTime,
 	}); err == nil {
 		t.Errorf("no error: there can be only one config (or even empty)")
+	}
+}
+
+func errorCreatPath(t *testing.T) {
+	path := ":?"
+
+	if _, err := Init(Config{
+		Path: path,
+	}); err == nil {
+		t.Errorf("no error: the directory name is invalid.")
 	}
 }
