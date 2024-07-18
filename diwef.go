53147739079493c73c5e11882996cf10773bcc8f
kuzminv <coolherringbone@gmail.com>
2024-07-18 13:17:45 +0700

    global visibility logger struct

--- diwef.go
+++ diwef.go
@@ -1,40 +1,40 @@
 package diwef
 
-type logger struct {
+type Logger struct {
 	writers []writer
 }
 
-func Init(writers ...writer) *logger {
-	return &logger{
+func Init(writers ...writer) *Logger {
+	return &Logger{
 		writers: writers,
 	}
 }
 
-func (l *logger) Debug(msg any) {
+func (l *Logger) Debug(msg any) {
 	for _, w := range l.writers {
 		w.debug(msg)
 	}
 }
 
-func (l *logger) Info(msg any) {
+func (l *Logger) Info(msg any) {
 	for _, w := range l.writers {
 		w.info(msg)
 	}
 }
 
-func (l *logger) Warning(msg any) {
+func (l *Logger) Warning(msg any) {
 	for _, w := range l.writers {
 		w.warning(msg)
 	}
 }
 
-func (l *logger) Error(msg any) {
+func (l *Logger) Error(msg any) {
 	for _, w := range l.writers {
 		w.error(msg)
 	}
 }
 
-func (l *logger) Fatal(msg any) {
+func (l *Logger) Fatal(msg any) {
 	for _, w := range l.writers {
 		w.fatal(msg)
 	}
